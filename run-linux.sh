d7436dd4baf6a24d00b7124d5f16a05f404b1c8e
Thomas Madlener <thomas.madlener@desy.de>
2025-04-11 16:25:51 +0200

    Remove capability to run from unpacked.cern.ch

--- run-linux.sh
+++ run-linux.sh
@@ -49,27 +49,13 @@ ${RUN}
 " > ${GITHUB_WORKSPACE}/action_payload.sh
 chmod a+x ${GITHUB_WORKSPACE}/action_payload.sh
 
-if [ ${UNPACKED} == "true" ]; then
-  echo "Install Singularity"
-  conda install --quiet --yes -c conda-forge singularity > /dev/null 2>&1
-  eval "$(conda shell.bash hook)"
-
-  echo "Starting Singularity image for ${SYSTEM} from /cvmfs/unpacked.cern.ch"
-  singularity instance start --bind /cvmfs --bind ${GITHUB_WORKSPACE}:${GITHUB_WORKSPACE} /cvmfs/unpacked.cern.ch/ghcr.io/aidasoft/${SYSTEM}:latest view_worker
-else
-  echo "Starting docker image for ${SYSTEM}"
-  docker run -it --name view_worker -v ${GITHUB_WORKSPACE}:${GITHUB_WORKSPACE} -v /cvmfs:/cvmfs:shared -d ghcr.io/aidasoft/${SYSTEM}:latest /bin/bash
-  echo "Docker image ready for ${SYSTEM}"
-fi
-
+echo "Starting docker image for ${SYSTEM}"
+docker run -it --name view_worker -v ${GITHUB_WORKSPACE}:${GITHUB_WORKSPACE} -v /cvmfs:/cvmfs:shared -d ghcr.io/aidasoft/${SYSTEM}:latest /bin/bash
+echo "Docker image ready for ${SYSTEM}"
 echo "::endgroup::" # Launch container
 
 echo "####################################################################"
 echo "###################### Executing user payload ######################"
 echo "VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV"
 
-if [ ${UNPACKED} == "true" ]; then
-  singularity exec instance://view_worker /bin/bash -c "cd ${GITHUB_WORKSPACE}; ./action_payload.sh"
-else
-  docker exec view_worker /bin/bash -c "cd ${GITHUB_WORKSPACE}; ./action_payload.sh"
-fi
+docker exec view_worker /bin/bash -c "cd ${GITHUB_WORKSPACE}; ./action_payload.sh"
